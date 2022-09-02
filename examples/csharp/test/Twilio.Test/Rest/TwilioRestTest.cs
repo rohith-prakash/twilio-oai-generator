5e5c8f5e009cd57cd03ac5631188f5dcdbecfe8e
Rohith Prakash <rprakash@twilio.com>
2022-09-02 16:05:10 +0530

    corrected awsresource

--- examples/csharp/test/Twilio.Test/Rest/TwilioRestTest.cs
+++ examples/csharp/test/Twilio.Test/Rest/TwilioRestTest.cs
@@ -25,15 +25,26 @@ namespace Twilio.Test.Rest
         public void TestShouldMakeValidAPICallAWSFetcher()
         {
             var twilioRestClient = Substitute.For<ITwilioRestClient>();
-            twilioRestClient.AccountSid.Returns("ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            twilioRestClient.Request(Arg.Any<Request>())
-                            .Returns(new Response(
-                                         System.Net.HttpStatusCode.OK,
-                                         "{\"account_sid\": \"ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"date_created\": \"2015-07-31T04:00:00Z\",\"date_updated\": \"2015-07-31T04:00:00Z\",\"friendly_name\": \"friendly_name\",\"sid\": \"CRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"url\": \"https://accounts.twilio.com/v1/Credentials/AWS/CRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"}"
-                                     ));
-
-            var response = AwsResource.Fetch("CRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", client: twilioRestClient);
-            Assert.NotNull(response);
+                    Request request = new Request(
+                            HttpMethod.Get,
+                            Twilio.Rest.Domain.Api,
+                            "/v1/Credentials/AWS"
+                    );
+                    string url = "https://api.twilio.com/v1/Credentials/AWS";
+                    String testResponse = "{\"credentials\":[{\"sid\":\"CR12345678123456781234567812345678\", \"test_string\":\"Ahoy\", \"test_object\":{\"mms\": true, \"sms\":false, \"voice\": false, \"fax\":true}}, {\"sid\":\"CR12345678123456781234567812345678\", \"test_string\":\"Hello\", \"test_object\":{\"mms\": true, \"sms\":false, \"voice\": false, \"fax\":true}}], \"meta\": {\"url\":\"" + url + "\", \"next_page_url\":\"" + url + "?PageSize=5" + "\", \"previous_page_url\":\"" + url + "?PageSize=3" + "\", \"first_page_url\":\"" + url + "?PageSize=1" + "\", \"page_size\":4}}";
+                    twilioRestClient.Request(request).Returns(new Response(System.Net.HttpStatusCode.OK, testResponse));
+                    var response = AwsResource.Read(client: twilioRestClient);
+                    Assert.IsNotNull(response);
+                    var awsEnumerator = response.GetEnumerator();
+                    Assert.IsNotNull(awsEnumerator);
+                    awsEnumerator.MoveNext();
+                    var firstResult = awsEnumerator.Current;
+                    Assert.IsNotNull(firstResult);
+                    Assert.AreEqual("CR12345678123456781234567812345678", firstResult.Sid);
+                    awsEnumerator.MoveNext();
+                    var secondResult = awsEnumerator.Current;
+                    Assert.IsNotNull(secondResult);
+                    Assert.AreEqual("CR12345678123456781234567812345678", secondResult.Sid);
         }
 
 
@@ -94,5 +105,76 @@ namespace Twilio.Test.Rest
             var response = AwsResource.Delete("CRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", client: twilioRestClient);
             Assert.NotNull(response);
         }
+
+        [Test]
+        public void TestAwsResourceUpdateResponse()
+        {
+            var twilioRestClient = Substitute.For<ITwilioRestClient>();
+            twilioRestClient.AccountSid.Returns("ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            twilioRestClient.Request(Arg.Any<Request>())
+                            .Returns(new Response(
+                                         System.Net.HttpStatusCode.OK,
+                                         "{\"account_sid\": \"ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"date_created\": \"2015-07-31T04:00:00Z\",\"date_updated\": \"2015-07-31T04:00:00Z\",\"friendly_name\": \"friendly_name\",\"sid\": \"CRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"url\": \"https://accounts.twilio.com/v1/Credentials/AWS/CRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"}"
+                                     ));
+
+            var response = AwsResource.Update("CRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", client: twilioRestClient);
+            Assert.NotNull(response);
+        }
+
+        [Test]
+       public void TestUpdateAwsResourceRequest()
+       {
+           var twilioRestClient = Substitute.For<ITwilioRestClient>();
+           var request = new Request(
+               HttpMethod.Post,
+               Twilio.Rest.Domain.Api,
+               "/v1/Credentials/AWS/CRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+               ""
+           );
+           twilioRestClient.Request(request).Throws(new ApiException("Server Error, no content"));
+           try
+           {
+               AwsResource.Update("CRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", client: twilioRestClient);
+               Assert.Fail("Expected TwilioException to be thrown for 500");
+           }
+           catch (ApiException) {}
+           twilioRestClient.Received().Request(request);
+       }
+
+       [Test]
+       public void TestAwsResourceReadRequest()
+       {
+           var twilioRestClient = Substitute.For<ITwilioRestClient>();
+           var request = new Request(
+               HttpMethod.Get,
+               Twilio.Rest.Domain.Api,
+               "/v1/Credentials/AWS",
+               ""
+           );
+           twilioRestClient.Request(request).Throws(new ApiException("Server Error, no content"));
+           try
+           {
+               AwsResource.Read(client: twilioRestClient);
+               Assert.Fail("Expected TwilioException to be thrown for 500");
+           }
+           catch (ApiException) {}
+           twilioRestClient.Received().Request(request);
+       }
+
+       [Test]
+       public void TestAwsResourceReadEmptyResponse()
+       {
+           var twilioRestClient = Substitute.For<ITwilioRestClient>();
+           twilioRestClient.AccountSid.Returns("ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+           twilioRestClient.Request(Arg.Any<Request>())
+                           .Returns(new Response(
+                                        System.Net.HttpStatusCode.OK,
+                                        "{\"credentials\": [],\"meta\": {\"first_page_url\": \"https://accounts.twilio.com/v1/Credentials/AWS?PageSize=50&Page=0\",\"key\": \"credentials\",\"next_page_url\": null,\"page\": 0,\"page_size\": 50,\"previous_page_url\": null,\"url\": \"https://accounts.twilio.com/v1/Credentials/AWS?PageSize=50&Page=0\"}}"
+                                    ));
+           var response = AwsResource.Read(client: twilioRestClient);
+           Assert.NotNull(response);
+       }
+
+
     }
 }
