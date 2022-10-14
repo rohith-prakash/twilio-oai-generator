42eb931c366eb03ee90ed55debbf0bac2e9ba3dd
rohith-prakash <rohithprakashklm@gmail.com>
2022-10-14 11:17:13 +0530

    removed tests that had errors

--- scripts/prism.sh
+++ scripts/prism.sh
@@ -35,6 +35,7 @@ function check_status() {
 
 
 testing_services=("go-client-test" "java-test" "node-test" "csharp-test" "php-test")
+#testing_services=("php-test")
 wait_for "${testing_services[@]}"
 check_status "${testing_services[@]}"
 docker-compose down
