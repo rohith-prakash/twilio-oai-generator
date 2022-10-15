053405c943692a78330433ebd1b46eac93588b6c
rohith-prakash <rohithprakashklm@gmail.com>
2022-10-15 20:17:03 +0530

    setting up monorepo

---
+++ examples/php/sonar-script/sonar.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#for ARGUMENT in "$@"
+#do
+#   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+#
+#   KEY_LENGTH=${#KEY}
+#   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+#
+#   export "$KEY"="$VALUE"
+#done
+
+sonar-scanner  \
+  -Dsonar.organization=rohith-prakash \
+  -Dsonar.projectKey=rohith-prakash_openapi-generator-php \
+  -Dsonar.sources=src/Twilio/Rest/**/*.* \
+  -Dsonar.tests=tests/Twilio/**/*.* \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login="${SONAR_TOKEN}" \
+  -Dsonar.language="php" \
+  -Dsonar.php.tests.reportPath="execution-result.xml" \
+  -Dsonar.php.coverage.reportPaths="php_coverage.xml"
+
