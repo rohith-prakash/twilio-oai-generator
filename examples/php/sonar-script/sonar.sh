a340b33f1713ccbf69c95789595f877bf75b9df1
rohith-prakash <rohithprakashklm@gmail.com>
2022-10-17 14:02:58 +0530

    changed source folder

--- examples/php/sonar-script/sonar.sh
+++ examples/php/sonar-script/sonar.sh
@@ -13,7 +13,7 @@ sonar-scanner -X  \
   -Dsonar.organization="rohith-prakash" \
   -Dsonar.projectKey="rohith-prakash_openapi-generator-php" \
   -Dsonar.projectName="rohith-prakash_openapi-generator-php" \
-  -Dsonar.sources="src" \
+  -Dsonar.sources="src/Twilio/Rest" \
   -Dsonar.tests="tests" \
   -Dsonar.host.url="https://sonarcloud.io" \
   -Dsonar.login=$SONAR_TOKEN \
