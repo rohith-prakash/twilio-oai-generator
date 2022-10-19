d4b854967e20ceeffa0602f2790e7919b5f2503d
rohith-prakash <rohithprakashklm@gmail.com>
2022-10-19 11:52:03 +0530

    additional test added

--- examples/php/tests/Twilio/Unit/Rest/ClientTest.php
+++ examples/php/tests/Twilio/Unit/Rest/ClientTest.php
@@ -257,4 +257,8 @@ class ClientTest extends UnitTest {
         $this->assertEquals($userAgentExtensions,$expectedExtensions);
     }
 
+    public function testForTestingSOnar(): void {
+            $this->assertEquals(1,1);
+        }
+
 }
